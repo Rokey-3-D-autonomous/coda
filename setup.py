76fb95ff4cdbb8e63f082f975be54d7f497ce9ea
hotteok00 <hojun7889@gmail.com>
2025-05-21 07:31:27 +0900

    fix: setup

--- setup.py
+++ setup.py
@@ -19,14 +19,20 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
+            "pub_test_detection_node = coda.pub_test_detection_node:main",
+            "pub_test_transform_node = coda.pub_test_transform_node:main",
+            "server_test = coda.server_test:main",
+            "server1_test = coda.server1_test:main",
+            "server1 = coda.server1:main",
+            "server2_node = coda.server2_node:main",
+            "turtlebot1_node = coda.turtlebot1_node:main",
+            "turtlebot2_node = coda.turtlebot2_node:main",
+            "wsh_nav2_controller = coda.wsh_nav2_controller:main",
+            "wsh_server1 = coda.wsh_server1:main",
+            "wsh_yolo_depth_rviz = coda.wsh_yolo_depth_rviz:main",
+            "part_transform = coda.server1.part_transform:main",   #여기부터 파일없음
             "part_detect = coda.server1.part_detect:main",
             "part_patrol = coda.part_patrol:main",
-            "part_transform = coda.server1.part_transform:main",
-            "server1_test = coda.server1.server1_test:main",
-            "server1 = coda.server1.server1:main",
-            "wsh_nav2_controller = coda.wsh_nav2_controller:main",
-            "wsh_server = coda.wsh_server1:main",
-            "server_test = coda.server_test:main",
         ],
     },
 )
